f52e407c3d255e12517bac2bb1523b2be157f0f2
Muhammad Hani <mohammadhani110@gmail.com>
2024-02-27 19:51:07 +0500

    project final screen

--- src/sections/project/project-final.js
+++ src/sections/project/project-final.js
@@ -1,20 +1,131 @@
 
-import { Paper, Typography, alpha } from '@mui/material'
+import { Avatar, Box, Card, Chip, Divider, ListItemText, Paper, Stack, Typography, alpha, styled } from '@mui/material'
 import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import { _bankingContacts, _mock } from 'src/_mock'
+import CustomBreadcrumbs from 'src/components/custom-breadcrumbs/custom-breadcrumbs'
+import Scrollbar from 'src/components/scrollbar'
+import { paths } from 'src/routes/paths'
 
 const ProjectFinal = () => {
     const [first, setfirst] = useState('second')
+    const { name, trades, workflow } = useSelector(state => state.project.create)
+    const { internal, external } = useSelector(state => state.project.inviteUsers)
+    const filteredTrades = trades.filter(item => "subcontractorId" in item);
+
+    const StyledCard = styled(Card, {
+        shouldForwardProp: (prop) => prop !== 'isSubcontractor',
+    })(({ isSubcontractor, theme }) => ({
+        "& .projectTitle": {
+            color: theme.palette.secondary.main, flex: .25,
+            borderRight: `2px solid ${alpha(theme.palette.grey[500], 0.12)}`
+        },
+        display: 'flex', borderRadius: "10px", padding: "1rem",
+        gap: '1rem',
+        ...(isSubcontractor && {
+            maxHeight: 300
+        }),
+
+    }));
     return (
-        <Paper
-            sx={{
-                p: 3,
-                my: 3,
-                minHeight: 120,
-                bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
-            }}
-        >
-            <Typography sx={{ my: 1 }}>Project Final View</Typography>
-        </Paper>
+        <>
+            <CustomBreadcrumbs
+                heading="Project Details"
+                links={[
+                    {
+                        name: 'Dashboard',
+                        // href: paths.subscriber.root
+                    },
+                    { name: 'Projects', href: paths.subscriber.submittals.list },
+                    { name: 'Prject' },
+                ]}
+
+                sx={{
+                    mt: { xs: 3, md: 5 },
+                    mb: { xs: 3 },
+                }}
+            />
+            <Divider sx={{
+                minHeight: '1px', bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
+                mb: 4
+            }} />
+            <Stack
+                sx={{
+                    my: 3,
+                    gap: 2
+                }}
+            >
+                <StyledCard>
+                    <Typography className='projectTitle' >Project Name</Typography>
+                    <Typography sx={{ color: (theme) => theme.palette.primary.main, flex: .75, px: 2 }}>{name}</Typography>
+                </StyledCard>
+                <StyledCard>
+                    <Typography className='projectTitle' >Project Trades</Typography>
+                    <Typography sx={{ color: (theme) => theme.palette.primary.main, flex: .15, px: 2 }}>Trades</Typography>
+                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5, flex: .6 }}>
+                        {trades?.length > 0 && trades?.slice(0, 4).map((item) => (
+                            <Chip key={item._id} size="small" variant='outlined' label={item.name} />
+                        ))}
+                        {trades?.length > 4 && (
+
+                            <Chip size="small" variant='outlined' label={`${trades.length - 4} +`} />
+                        )
+
+                        }
+
+                    </Box>
+                </StyledCard>
+                <StyledCard>
+                    <Typography className='projectTitle' >Project Workflow</Typography>
+                    <Typography sx={{ color: (theme) => theme.palette.primary.main, flex: .75, px: 2 }}>{workflow?.name}</Typography>
+                </StyledCard>
+                {filteredTrades?.length > 0 && <StyledCard isSubcontractor>
+                    <Typography className='projectTitle' >Subcontractors</Typography>
+                    <Stack spacing={3} sx={{ flex: .75 }}>
+                        <Scrollbar>
+                            <Stack spacing={3} maxHeight={300}>
+                                {/* _bankingContacts.slice(-5) */}
+                                {filteredTrades.map((trade, index) => (
+                                    <Stack direction="row" alignItems="center" key={trade.id}>
+                                        {trade?.subcontractorId && <Avatar src={_mock.image.avatar(index)} sx={{ width: 48, height: 48, mr: 2 }} />}
+
+                                        <ListItemText primary={trade.name} secondary={trade.subcontractorId} />
+                                        {!trade?.subcontractorId && <Chip size="small" variant='outlined' label='N/A' />}
+                                    </Stack>
+                                ))}
+                            </Stack>
+                        </Scrollbar>
+                    </Stack>
+                </StyledCard>}
+                {((external?.length > 0) || (internal?.length > 0)) && <StyledCard>
+                    <Typography className='projectTitle'>Invited Users</Typography>
+                    <Stack spacing={3} sx={{ flex: .75 }}>
+                        <Scrollbar>
+                            <Stack spacing={3} maxHeight={300}>
+                                {/* _bankingContacts.slice(-5) */}
+                                {(internal?.length > 0) && internal.map((user, index) => (
+                                    <Stack direction="row" alignItems="center" key={user._id}>
+                                        <Avatar src={_mock.image.avatar(index)} sx={{ width: 48, height: 48, mr: 2 }} />
+
+                                        <ListItemText primary={user.role} secondary={user.email} />
+                                        {/* {!trade?.subcontractorId && <Chip size="small" variant='outlined' label='N/A' />} */}
+                                    </Stack>
+                                ))}
+                                {(external?.length > 0) && external.map((user, index) => (
+                                    <Stack direction="row" alignItems="center" key={user._id}>
+                                        <Avatar src={_mock.image.avatar(index)} sx={{ width: 48, height: 48, mr: 2 }} />
+
+                                        <ListItemText primary={user.role} secondary={user.email} />
+                                        {/* {!trade?.subcontractorId && <Chip size="small" variant='outlined' label='N/A' />} */}
+                                    </Stack>
+                                ))}
+                            </Stack>
+                        </Scrollbar>
+                    </Stack>
+                </StyledCard>}
+
+            </Stack>
+        </>
     )
 }
 
--- src/sections/project/project-invite-users.js
+++ src/sections/project/project-invite-users.js
@@ -1,61 +1,20 @@
-import React, { useCallback, useState } from 'react'
-// mui
-import { Box, Button, Divider, FormControlLabel, Radio, Stack, Typography, alpha } from '@mui/material'
-//
-import { RHFRadioGroup } from 'src/components/hook-form'
+import { Box,  Divider, Typography, alpha } from '@mui/material'
 // components
-import Iconify from 'src/components/iconify'
-import ProjectInviteUserOutside from './project-invite-user-outside'
 import ProjectInviteUserStudly from './project-invite-user-studly'
-import ProjectInviteNewUser from './project-invite-new-user'
-import ProjectInviteUserDialog from './project-invite-user-dialog'
 
-const ProjectInviteUsers = () => {
-    const [openInviteUser, setOpenInviteUser] = useState(false)
-    // // const [inviteEmail, setInviteEmail] = useState('')
-    // // const handleOptionChange = (event) => {
-    // //     setSelectedOption(event.target.value);
-    // // };
-    // // const handleChangeInvite = useCallback((event) => {
-    // //     const email = event.target.value;
-    // //     setInviteEmail(email);
-
-    //     // Validate email format
-    // //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // //     const isValidEmail = emailRegex.test(email);
-
-    // //     if (!isValidEmail) {
-    // //         setError(true);
-    // //         setHelperText('Invalid email format');
-    // //     } else {
-    // //         setError(false);
-    // //         setHelperText('');
-    // //     }
-    // // }, []);
+const ProjectInviteUsers = () => (
+    <>
+        <Box sx={{ mt: 1, mb: 2, display: 'flex', justifyContent: 'space-between' }}>
+            <Typography fontSize='1.5rem' fontWeight='bold'>Invite Users</Typography>
+        </Box>
+        <Divider sx={{
+            minHeight: '1px', bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
+            mb: 7
+        }} />
 
 
-    // // const handleSendInvite = () => {
-    //     // Handle sending invite
-    // //     console.log('email', inviteEmail)
-    // //     setInviteEmail('')
-    // //     // snackbar
-    // //     setOpenInviteUser(false)
-    // // };
-
-    return (
-        <>
-            <Box sx={{ mt: 1, mb: 2, display: 'flex', justifyContent: 'space-between' }}>
-                <Typography fontSize='1.5rem' fontWeight='bold'>Invite Users</Typography>
-            </Box>
-            <Divider sx={{
-                minHeight: '1px', bgcolor: (theme) => alpha(theme.palette.grey[500], 0.12),
-                mb: 7
-            }} />
-
-
-            <ProjectInviteUserStudly />
-        </>
-    )
-}
+        <ProjectInviteUserStudly />
+    </>
+)
 
 export default ProjectInviteUsers
