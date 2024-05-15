2487e983190d62814e5eeb6bc2a2b73b4652ed95
Muhammad Hani <mohammadhani110@gmail.com>
2024-05-15 15:41:14 +0500

    submit to architect in edit submittal

--- src/sections/subscriber/submittals/view/submittals-edit-view.js
+++ src/sections/subscriber/submittals/view/submittals-edit-view.js
@@ -1,18 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 //
 import { useDispatch, useSelector } from 'react-redux';
 // @mui
+import { isEmpty } from 'lodash';
+import { LoadingButton } from '@mui/lab';
+import Box from '@mui/material/Box';
+import { useSnackbar } from 'notistack';
 import Container from '@mui/material/Container';
 // routes
 import { paths } from 'src/routes/paths';
 // _mock
-import { _userList } from 'src/_mock';
+import { SUBSCRIBER_USER_ROLE_STUDLY, _userList } from 'src/_mock';
 // components
 import { useSettingsContext } from 'src/components/settings';
 import CustomBreadcrumbs from 'src/components/custom-breadcrumbs';
 //
-import { setCurrentSubmittal } from 'src/redux/slices/submittalSlice';
+import { getSubmittalDetails, getSubmittalList, setCurrentSubmittal, submitSubmittalToArchitect } from 'src/redux/slices/submittalSlice';
 import SubmittalsNewEditForm from '../submittals-new-edit-form';
 
 // ----------------------------------------------------------------------
@@ -23,8 +27,15 @@ export default function SubmittalsEditView({ id }) {
   const submittalList = useSelector(state => state.submittal?.list?.docs)
   const dispatch = useDispatch()
   const currentSubmittal = submittalList?.find(item => item.id === id)
+  // const currentSubmittal = useSelector((state) => state.submittal.current);
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
+  const currentUser = useSelector((state) => state.user?.user);
+
   console.log("currentSubmittal", currentSubmittal)
   console.log("sumittalId", id)
+  
   useEffect(() => {
     if (id) {
       dispatch(setCurrentSubmittal(currentSubmittal))
@@ -32,7 +43,20 @@ export default function SubmittalsEditView({ id }) {
 
   }, [dispatch, id, currentSubmittal])
 
-
+  const handleSubmitToArchitect = async () => {
+    console.log('SubmittalId', id);
+    setIsSubmitting(true);
+    const { error, payload } = await dispatch(submitSubmittalToArchitect(id));
+    console.log('e-p', { error, payload });
+    setIsSubmitting(false);
+    if (!isEmpty(error)) {
+      enqueueSnackbar(error.message, { variant: 'error' });
+      return;
+    }
+    enqueueSnackbar('Submittal submitted to architect successfully', { variant: 'success' });
+    await dispatch(getSubmittalList({ search: '', page: 1, status: [] }));
+    await dispatch(getSubmittalDetails(id));
+  };
   return (
     <Container maxWidth={settings.themeStretch ? false : 'lg'}>
       <CustomBreadcrumbs
@@ -52,7 +76,21 @@ export default function SubmittalsEditView({ id }) {
           mb: { xs: 3, md: 5 },
         }}
       />
-
+      {currentSubmittal?.status === 'Draft' &&
+        (currentUser?.role?.name === SUBSCRIBER_USER_ROLE_STUDLY.CAD ||
+          currentUser?.role?.name === SUBSCRIBER_USER_ROLE_STUDLY.PWU) && (
+          <Box width="100%" display="flex" justifyContent="end">
+            <LoadingButton
+              type="submit"
+              variant="contained"
+              size="large"
+              loading={isSubmitting}
+              onClick={handleSubmitToArchitect}
+            >
+              Submit to Architect
+            </LoadingButton>
+          </Box>
+        )}
       <SubmittalsNewEditForm currentSubmittal={currentSubmittal} id={id} />
     </Container>
   );
