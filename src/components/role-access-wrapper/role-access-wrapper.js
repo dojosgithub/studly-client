3f159ba7da196fe964fc4f18dd4d13145ef8f988
Muhammad Hani <mohammadhani110@gmail.com>
2024-08-20 19:04:55 +0500

    role access wrapper

--- src/components/role-access-wrapper/role-access-wrapper.js
+++ src/components/role-access-wrapper/role-access-wrapper.js
@@ -2,20 +2,23 @@ import { jwtDecode } from 'jwt-decode';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
-const RoleAccessWrapper = ({ children }) => {
-  const userRole = useSelector((state) => state.user.user.role);
+const RoleAccessWrapper = ({ allowedRoles = [], children, isSysAdmin = false }) => {
+  const userRole = useSelector((state) => state.user.user.role.shortName);
   const accessToken = useSelector((state) => state.user.tokens.accessToken);
   const decodedToken = jwtDecode(accessToken);
-  const authRole = decodedToken.role;
+  const authRole = decodedToken.role.shortName;
   console.log('decodedToken', decodedToken);
   console.log('authRole', authRole);
   console.log('userRole', userRole);
-  console.log('matched', authRole.shortName === userRole.shortName);
-  return authRole.shortName === userRole.shortName ? children : null;
+  console.log('matched', allowedRoles.includes(authRole));
+
+  return allowedRoles.includes(authRole) ? children : null;
 };
 
 export default RoleAccessWrapper;
 
 RoleAccessWrapper.propTypes = {
   children: PropTypes.node,
+  isSysAdmin: PropTypes.bool,
+  allowedRoles: PropTypes.array,
 };
