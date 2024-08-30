5cd54cdcf62b0f4cecdacfab99d78d289334d7b2
Raahim <raahimhussain1420@gmail.com>
2024-08-30 16:43:37 +0500

    logo fixed 2

--- src/sections/subscriber/auth/update-password/subscirber-update-password-view.js
+++ src/sections/subscriber/auth/update-password/subscirber-update-password-view.js
@@ -6,7 +6,7 @@ import Box from '@mui/material/Box';
 import { useSettingsContext } from 'src/components/settings';
 //
 
-import Logo from 'src/components/logo/logo';
+import Logo from 'src/components/logo/logo1';
 import SubscriberUpdatePassword from './subscriber-update-password';
 
 // ----------------------------------------------------------------------
@@ -14,24 +14,25 @@ import SubscriberUpdatePassword from './subscriber-update-password';
 // ----------------------------------------------------------------------
 
 export default function SubscriberUpdatePasswordView() {
-    const settings = useSettingsContext();
+  const settings = useSettingsContext();
 
-
-    return (
-        <Container sx={{ height: "100%" }} maxWidth={settings.themeStretch ? false : 'lg'}>
-            <Box sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "100%",
-                gap: "2rem"
-            }}>
-                <Box>
-                    <Logo sx={{ width: "100%", height: '2.5rem', }} />
-                </Box>
-                <SubscriberUpdatePassword />
-            </Box>
-        </Container>
-    );
+  return (
+    <Container sx={{ height: '100%' }} maxWidth={settings.themeStretch ? false : 'lg'}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+          gap: '2rem',
+        }}
+      >
+        <Box>
+          <Logo sx={{ width: '150px', height: '4rem' }} />
+        </Box>
+        <SubscriberUpdatePassword />
+      </Box>
+    </Container>
+  );
 }
