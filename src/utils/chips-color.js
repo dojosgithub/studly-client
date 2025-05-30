f2989919009c64323dd019e7c247fa8784ff846f
dojosgithub <dojo.developments@gmail.com>
2025-05-30 15:07:15 +0500

    hover bug fix

--- src/utils/chips-color.js
+++ src/utils/chips-color.js
@@ -3,22 +3,61 @@
 export const getStatusStyle = (status) => {
   switch (status) {
     case 'Draft':
-      return { bgcolor: '#919EAB33', color: '#A8A9AD' }; // orange
+      return {
+        bgcolor: '#919EAB33',
+        color: '#A8A9AD',
+        '&:hover': {
+          bgcolor: '#919EAB33',
+          color: '#A8A9AD',
+        },
+      };
     case 'Submitted':
-      return { bgcolor: '#22C55E29', color: '#118D57' }; // green
+      return {
+        bgcolor: '#22C55E29',
+        color: '#118D57',
+        '&:hover': {
+          bgcolor: '#22C55E29',
+          color: '#118D57',
+        },
+      };
     case 'Reviewed for Record':
-      return { bgcolor: '#00B8D929', color: '#006C9C' }; // red
     case 'Approved (APR)':
-      return { bgcolor: '#00B8D929', color: '#006C9C' }; // red
     case 'Reviewed':
-      return { bgcolor: '#00B8D929', color: '#006C9C' }; // red
+      return {
+        bgcolor: '#00B8D929',
+        color: '#006C9C',
+        '&:hover': {
+          bgcolor: '#00B8D929',
+          color: '#006C9C',
+        },
+      };
     case 'Make Corrections Noted (MCN)':
-      return { bgcolor: '#FFAB0029', color: '#B76E00' }; // red
     case 'Make Corrections and Resubmit (MCNR)':
-      return { bgcolor: '#FFAB0029', color: '#B76E00' }; // red
+      return {
+        bgcolor: '#FFAB0029',
+        color: '#B76E00',
+        '&:hover': {
+          bgcolor: '#FFAB0029',
+          color: '#B76E00',
+        },
+      };
     case 'Rejected (RJT)':
-      return { bgcolor: '#FF563029', color: '#B71D18' }; // red
+      return {
+        bgcolor: '#FF563029',
+        color: '#B71D18',
+        '&:hover': {
+          bgcolor: '#FF563029',
+          color: '#B71D18',
+        },
+      };
     default:
-      return { bgcolor: '#E0E0E0', color: '#000000' }; // grey
+      return {
+        bgcolor: '#E0E0E0',
+        color: '#000000',
+        '&:hover': {
+          bgcolor: '#E0E0E0',
+          color: '#000000',
+        },
+      };
   }
 };
