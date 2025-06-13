1346900542c18923a5555cf46221e9055a76c6c8
dojosgithub <dojo.developments@gmail.com>
2025-06-13 18:46:11 +0500

    adding jwt login page to the route in main.js

--- src/routes/sections/main.js
+++ src/routes/sections/main.js
@@ -10,7 +10,7 @@ const Page500 = lazy(() => import('src/pages/500'));
 const Page403 = lazy(() => import('src/pages/403'));
 const Page404 = lazy(() => import('src/pages/404'));
 const InvitePage = lazy(() => import('src/pages/invite'));
-
+const JwtLoginPage = lazy(() => import('src/pages/auth/jwt/login'));
 // ----------------------------------------------------------------------a
 
 export const mainRoutes = [
@@ -22,6 +22,7 @@ export const mainRoutes = [
     ),
     children: [
       { path: 'invite/:inviteId', element: <InvitePage /> },
+      { path: 'auth/jwt/login', element: <JwtLoginPage /> },
       { path: '500', element: <Page500 /> },
       { path: '404', element: <Page404 /> },
       { path: '403', element: <Page403 /> },
